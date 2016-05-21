78055f4cd8784c17eb65c7a9a7a0553c3be412ff
root <root@a-micron.ru>
2016-05-21 14:34:47 +0300

    Install ACL

--- config/app.php
+++ config/app.php
@@ -156,6 +156,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Vendor Provider
+         */
+        'LaravelAcl\Authentication\AuthenticationServiceProvider',
+
     ],
 
     /*
