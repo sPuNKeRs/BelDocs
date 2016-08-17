c5f33e3db4fc97227a5aaa08027a178cc4a77b35
PuNKeR <maximkoritin@gmail.com>
2016-08-17 10:17:13 +0300

    17-08-2016

---
+++ database/migrations/2016_08_16_151119_create_responsible_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateResponsibleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('responsible', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('entity_id');
+            $table->string('entity_slug');
+            $table->string('entity_type');
+            $table->integer('user_id');
+            $table->date('executed_at');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('responsible');
+    }
+}
