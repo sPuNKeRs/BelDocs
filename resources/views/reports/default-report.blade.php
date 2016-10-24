f55644fdc22ba3915f3bb8c8c63e411243bc2928
PuNKeR <maximkoritin@gmail.com>
2016-10-24 22:01:20 +0300

    вид отчетов

---
+++ resources/views/reports/default-report.blade.php
@@ -0,0 +1,38 @@
+<div class="panel panel-default grid">
+  <div class="panel-heading">
+    <i class='fa fa-table fa-lg'></i> Результат
+    <div class="panel-tools">
+      <div class="badge">3 записи</div>
+    </div>
+  </div>
+  <table class="table table-condensed">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Username</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Larry</td>
+        <td>the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -5,23 +5,40 @@
 @section('keywords', '')
 
 @section('pageClass', 'main page')
-@include('partials.navbar')  
+@include('partials.navbar')
 @section('content')
-    
+
     <div id='wrapper'>
       @include('partials.sidebar')
       @include('partials.tools')
       <!-- Content -->
       <div id='content'>
+        <!--Параметры отчета-->
         <div class='panel panel-default'>
           <div class='panel-heading'>
             <i class='fa fa-wpforms fa-lg'></i>
-            Отчеты          
+            Отчеты
           </div>
-          <div class='panel-body'>            
+          <div class='panel-body'>
+              <form>
+              <fieldset>
+                <legend>Параметры отчета</legend>
+              </fieldset>
+              <div class="form-actions">
+                <button class="btn btn-default" type="submit">Сформировать</button>
+                <button class="btn btn-default">Очистить</button>
+              </div>
+            </form>
           </div>
         </div>
+        <!--Параметры отчета - конец-->
+
+        <!--Результат-->
+
+        <!--Стандартный отчет-->
+        @include('reports.default-report')
+
+        <!--Результат - конец-->
       </div>
     </div>
 @stop
-
